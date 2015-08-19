ff6a98c052dd1bc2cdc7ffbdbde900c9ce34c962
marcus <marcus@essenmitsosse.de>
2015-08-19 12:42:15 +0200

    non-minified files when in script_debug mode

--- includes/base/scripts-and-styles.php
+++ includes/base/scripts-and-styles.php
@@ -2,9 +2,14 @@
 
 add_action( 'wp_enqueue_scripts', 'add_style_and_script' );
 function add_style_and_script() {
+	if( SCRIPT_DEBUG ) {
+		$file_extension = '';
+	} else {
+		$file_extension = '.min';
+	}
 
 	// STYLES
-	wp_enqueue_style( 'theme-style',  get_stylesheet_directory_uri() . '/style.min.css');
+	wp_enqueue_style( 'theme-style',  get_stylesheet_directory_uri() . '/style' . $file_extension . '.css');
 	add_action('wp_head', 'no_javascript_style', 100);
 		// wp_deregister_style( 'contact-form-7', 1 );
 
@@ -14,8 +19,8 @@ function add_style_and_script() {
 	wp_enqueue_script( 'html5shiv', 'http://html5shim.googlecode.com/svn/trunk/html5.js', false, '3.7.0', false );
 
 	// FOOTER SCRIPTS
-	wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js', false, '2.1.4', true );
-	wp_enqueue_script( 'theme-script', get_stylesheet_directory_uri() . '/assets/js/main.js', array( 'jquery' ), '0.1.0', true );
+	wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery' . $file_extension . '.js', false, '2.1.4', true );
+	wp_enqueue_script( 'theme-script', get_stylesheet_directory_uri() . '/assets/js/main' . $file_extension . '.js', array( 'jquery' ), '0.1.0', true );
 	
 }
 
